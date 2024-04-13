f0478d3cfef17394081e8a93fe800d3112707b52
Muhammad Moneib <mmoneib@gmx.com>
2024-04-13 23:51:26 +0200

    Added added adversary agent to numbers_balloon, adjusted the boundaries to wrap around the screen.

--- numbers_balloon.sh
+++ numbers_balloon.sh
@@ -8,8 +8,7 @@
 # Conceptualized and Developed by: Muhammad Moneib                             #
 ################################################################################
 
-#TODO Add X to represent birds and end the game if it hits the upper part of the baloon.
-#TODO Adjust speeds.
+#TODO Fix balloon interaction with statistics line.
 #TODO Add input options.
 
 function initialize {
@@ -25,14 +24,17 @@ function initialize {
   clockSpeed="$(echo "scale=2;(1-($(( (difficulty-1)*2 ))/10))/10"|bc -l|sed "s/^\./0\./g")" # Internal difficulty.
   linesBetweenNumbers=5
   y=$(( $numOfLines-10 )) # Start at lower part vertically.
-  x=$(( $numOfCols/2 )) # Start at center horizontally.
+  x=$(( $numOfCols/2 )) # Start at center horizontally.	
   iteration=0
   score=0 # Needed if the score is not the time.
-frame=0
+  iterationsPerFrame=8
+  frame=0
   # Objects Definition
   balloonChar="|"
   basketChar="#"
   spaceChar=" "
+  numbers=(1 2 3 4 5 6 7 8 9 "X")
+  numbersSize=${#numbers[@]}
   staticStatistics="Numbers Balloon by Muhammad Moneib - Difficulty: $difficulty - Score: "
   ballonGrid=() # Contains the positions forming the plane layer to be applied over the background grid.
   basketGrid=()
@@ -42,16 +44,16 @@ frame=0
   done
   # Signal Traps
   function exitFunc {
+    [ ! $? -eq 0 ] && echo "Error occured! :-(" && exit 1 # $? has to be in the first line of the function to get the last's exit code.
     tput clear
     echo " GGG   AAA  MM MM EEEEE        OOO  V   V EEEEE RRRR 
 G     A   A M M M E           O   O V   V E     R   R
 G GGG AAAAA M M M EEEE        O   O V   V EEEE  RRRR 
 G   G A   A M   M E           O   O  V V  E     R   R
- GGG  A   A M   M EEEEE        OOO    V   EEEEE R   R
-"
+ GGG  A   A M   M EEEEE        OOO    V   EEEEE R   R"
     echo "$statistics" # Calculated by the engine.
   }
-  trap exitFunc EXIT # TODO Escape errors from trap.
+  trap exitFunc EXIT
   # Splash Screen
   splashLines=("N   N U   U MM MM BBBB  EEEEE RRRR   SSSS       BBBB   AAA  L     L      OOO   OOO  N   N"
                "NN  N U   U M M M B   B E     R   R S           B   B A   A L     L     O   O O   O NN  N"
@@ -79,7 +81,7 @@ function engine {
       for (( p=0; p<$(( numberPositionInLine-1 )); p++ )); do
         newLineUp+="$spaceChar"
       done
-      newLineUp+="$(( RANDOM%10 ))"
+      newLineUp+="${numbers[$(( RANDOM%numbersSize ))]}"
       for (( p=$numberPositionInLine; p<$numOfCols; p++ )); do
         newLineUp+="$spaceChar"
       done
@@ -91,10 +93,9 @@ function engine {
     backGrid="$newLineUp$backGrid" # Add new line on top of the background grid.
     backGrid="${backGrid:0:$numOfPixels}" # Trim the extra line at the bottom.
     # Boundary Rules
-    [ $(( x+4 )) -ge $(( numOfCols-1)) ] && x=$(( x-2 )) # 5 is the greatest distance from x to the right based on the grid below. It is the width of the plane.
-    [ $(( x-4 )) -lt 0 ] && x=$(( x+2 ))
-    [ $(( y+4 )) -ge $(( numOfLines)) ] && y=$(( y-2 ))
-    # ForegroundLayer
+    y=$(( (y+numOfLines)%numOfLines )) # Wrap to the other side of the screen. The addition is for the case where y becomes negative.
+    x=$(( (x+numOfCols)%numOfCols )) # Wrap to the other side of the screen. The addition is for the case where x becomes negative.
+    # Foreground Layer
     function frame0 {
       balloonGrid=($(( y*numOfCols+x )) $(( y*numOfCols+x+1 )) $(( y*numOfCols+x+2 )) $(( y*numOfCols+x+3 )) $(( y*numOfCols+x-1 )) $(( y*numOfCols+x-2 )) $(( y*numOfCols+x-3 ))
 $(( (y-1)*numOfCols+x )) $(( (y-1)*numOfCols+x+1 )) $(( (y-1)*numOfCols+x+2 )) $(( (y-1)*numOfCols+x+3 )) $(( (y-1)*numOfCols+x+4 )) $(( (y-1)*numOfCols+x-1 )) $(( (y-1)*numOfCols+x-2 )) $(( (y-1)*numOfCols+x-3 )) $(( (y-1)*numOfCols+x-4 ))
@@ -137,11 +138,14 @@ $(( (y+4)*numOfCols+x-1 )) $(( (y+4)*numOfCols+x-2 ))
     function frame3 { # To close the loop. 
       frame1 
     }
-    [ $((iteration&15)) -eq 0 ] && frame=$(( (frame+1)%4 ))
+    [ $((iteration%$iterationsPerFrame)) -eq 0 ] && frame=$(( (frame+1)%4 ))
     frame$frame # Polymorphism through functional reference.
     # Merge of State + Collision Rules
     allGrid="$backGrid" # A new layer is needed to avoid mixing up the top layer in the dynamic of the background layer with the next iteration.
+    exitAfterPrint=false
     for (( a=0; a<${#balloonGrid[@]}; a++ )); do
+      backGridChar="${backGrid:${balloonGrid[$a]}:1}"
+      [ "$backGridChar" == "X" ] && allGrid=${allGrid:0:${balloonGrid[$a]}}"$backGridChar"${allGrid:$(( ${balloonGrid[$a]}+1 ))} && exitAfterPrint=true && continue # The flow makes sense so as the puncture is shown graphically and any simultaneous scoring is calculated before exiting.
       allGrid=${allGrid:0:${balloonGrid[$a]}}"$balloonChar"${allGrid:$(( ${balloonGrid[$a]}+1 ))}
     done
     for (( a=0; a<${#basketGrid[@]}; a++ )); do
@@ -156,18 +160,19 @@ $(( (y+4)*numOfCols+x-1 )) $(( (y+4)*numOfCols+x-2 ))
     done
     tput cup 0 0 # Write over the previous frame, faster than tput reset.
     echo -e "$allGrid$statistics" # Can be commented out for debugging. Echo seems faster than printf.
-    # Clock + User Interaction
+    [ $exitAfterPrint == true ] && printf "\a" && sleep 1 && printf "\a" && sleep 1 && printf "\a" && sleep 1 && exit
+    # Clock + User Interaction + Boundary Rules
     function up {
-      y=$((y-2))
+      y=$(( y-2 ))
     }
     function left {
-      x=$((x-2))
+      x=$(( x-4 )) # More than the vertical displacement due to landscape frame. Displacement is used instead of a logical grid.
     }
     function down {
-      y=$((y+2))
+      y=$(( y+2 ))
     }
     function right {
-      x=$((x+2))
+      x=$(( x+4 )) # More than the vertical displacement due to landscape frame. Displacement is used instead of a logical grid.
     }
     function upLeft {
       up
@@ -186,7 +191,7 @@ $(( (y+4)*numOfCols+x-1 )) $(( (y+4)*numOfCols+x-2 ))
       right
     }
     iteration=$(( iteration+1 ))
-    read -sn 6 -t $clockSpeed inp # Clocking integrated in read command.
+    read -sn 6 -t $clockSpeed inp # Clocking integrated in read command. 6 is 2 read 2 buttons.
     case $inp in
       $'\033[A') move="up" ;; # Up
       $'\033[D') move="left" ;; # Left
@@ -200,7 +205,7 @@ $(( (y+4)*numOfCols+x-1 )) $(( (y+4)*numOfCols+x-2 ))
       $'\033[D\033[B') move="downLeft" ;;
       $'\033[B\033[C') move="downRight" ;;
       $'\033[C\033[B') move="downRight" ;;
-      *) move="$execMove" ;; # Needed to get the move from the previous iteration.
+      *) move="$execMove" ;; # Needed to get the move from the previous iteration, for inertia.
     esac
     execMove="$move" # Without this, somehow, move doesn't survive to the next iteration.
     $execMove # Polymorphism through functional calls.
